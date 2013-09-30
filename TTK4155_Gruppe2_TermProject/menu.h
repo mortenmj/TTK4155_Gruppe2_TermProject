617f070d55a20cdfbb4f161881d569fa11955860
Magne Alver Normann <magnealvnor@gmail.com>
2013-09-29 20:59:53 -0700

    added menu_update()

--- TTK4155_Gruppe2_TermProject/menu.h
+++ TTK4155_Gruppe2_TermProject/menu.h
@@ -2,13 +2,14 @@
  * menu.h
  *
  * Created: 25.09.2013 21:11:36
- *  Author: mortenmj
+ *  Author: mortenmj & magnealv
  */ 
 
 #ifndef MENU_H_
 #define MENU_H_
 
 #include <stdint.h>
+#include "joystick.h"
 
 #define MAX_TITLE 14
 #define MAX_SUBMENUS 16
@@ -19,6 +20,7 @@ typedef struct menu_item
 	void (*command)();
 	
 	uint8_t num_submenus;
+	uint8_t child_selected;
 	struct menu_item *parent;
 	struct menu_item *children[MAX_SUBMENUS];
 } menu_t;
@@ -26,6 +28,6 @@ typedef struct menu_item
 menu_t menu_create (void);
 void menu_destroy (void);
 void menu_display (menu_t *m);
-void menu_navigate (menu_t *m);
+menu_t* menu_update (menu_t *m, direction_t joystick, uint8_t button);
 
 #endif /* MENU_H_ */
