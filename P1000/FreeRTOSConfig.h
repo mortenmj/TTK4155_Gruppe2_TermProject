a2895529f1682e2603f8aee104e38dadf22f5f26
Morten Mjelva <morten.mjelva@gmail.com>
2013-11-05 17:33:20 +0100

    Enable mutex

--- P1000/FreeRTOSConfig.h
+++ P1000/FreeRTOSConfig.h
@@ -94,6 +94,7 @@
 #define configUSE_16_BIT_TICKS		1
 #define configIDLE_SHOULD_YIELD		1
 #define configQUEUE_REGISTRY_SIZE	0
+#define configUSE_MUTEXES			1
 
 /* Hook function related definitions */
 #define configUSE_IDLE_HOOK				1
