2ffa22a057bff5af18739458ae7542781bfd15ab
Morten Mjelva <morten.mjelva@gmail.com>
2013-11-20 05:49:05 +0100

    Documented Score

--- P1000/Display/score.h
+++ P1000/Display/score.h
@@ -1,3 +1,11 @@
+/**
+ * @file
+ *
+ * @ingroup Score
+ *
+ * @brief Score sub-module
+ */
+
 /*
  * score.h
  *
@@ -9,14 +17,59 @@
 #ifndef SCORE_H_
 #define SCORE_H_
 
+/**
+ * @defgroup Score Score module
+ *
+ * @ingroup Display
+ *
+ * @brief Provides functions for keeping score
+ *
+ * @{
+ */
+
+/**
+ * @brief Initialize scorekeeping
+ */
+
+/**
+ * @brief Initialize menu
+ */
 void score_init (void);
+
+/**
+ * @brief Function to display the score
+ *
+ * @param u8g The display device
+ */
 void score_display (u8g_t *u8g);
+
+/**
+ * @brief Function to display the highscore screen
+ *
+ * @param u8g The display device
+ */
 void score_hiscore_display (u8g_t *u8g);
 
+/**
+ * @brief Start the score counter
+ */
 void score_start (void);
+
+/**
+ * @brief Reset the score counter
+ */
 void score_reset (void);
 
+/**
+ * @brief Save highscore to persistent memory
+ */
 void score_save (void);
+
+/**
+ * @brief Clear highscore from persistent memory
+ */
 void score_clear (void);
+
+/** @} */
 
 #endif /* SCORE_H_ */
