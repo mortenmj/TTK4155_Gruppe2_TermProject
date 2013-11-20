d98041f4776bc82a20255bd3a25826687bb1a8aa
Morten Mjelva <morten.mjelva@gmail.com>
2013-11-20 05:41:15 +0100

    Documented Display

--- P1000/Display/display.h
+++ P1000/Display/display.h
@@ -11,9 +11,36 @@
 
 #include "u8glib/u8g.h"
 
+/**
+ * @brief The main function for the Display module
+ *
+ * @param[in] pvParameters Parameter for the task being created. See FreeRTOS documentation for details.d
+ *
+ */
 void vDisplay ( void *pvParameters );
+
+/**
+ * @brief Convenience function to retrieve the task handle.
+ *
+ * @return The xTaskHandle for the Display task.
+ *
+ */
 xTaskHandle xDisplayGetHandle (void);
+
+/**
+ * @brief Convenience function to set the task handle.
+ *
+ * @param handle The xTaskHandle for the Display task.
+ *
+ */
 void vDisplaySetHandle (xTaskHandle handle);
+
+/**
+ * @brief Set the display handle function.
+ *
+ * @param func The function to use for updating the display.
+ *
+ */
 void vDisplaySetDisplayFunc (void (*func)(u8g_t *u8g));
 
 #endif /* DISPLAY_H_ */
